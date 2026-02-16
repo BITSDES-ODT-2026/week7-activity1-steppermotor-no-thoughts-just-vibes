1999777cc6346d1731e766e2426aa84b0d007add
Parajika <130729432+no-thoughts-just-vibes@users.noreply.github.com>
2026-02-16 14:10:34 +0530

    Update lighting pattern for stepper motor control

--- StepperMotor-Advanced.py
+++ StepperMotor-Advanced.py
@@ -4,7 +4,7 @@ s1=Pin(4,Pin.OUT)
 s2=Pin(14,Pin.OUT)
 s3=Pin(18,Pin.OUT)
 s4=Pin(19,Pin.OUT)
-lit=[[1,1,0,0],[0,1,1,0],[0,0,1,1],[1,0,0,1]]
+lit=[[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
 while True:
     for i in lit:
         s1.value(i[0])
